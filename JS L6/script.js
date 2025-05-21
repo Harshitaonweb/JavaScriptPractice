05efdbaf263aa2afd0b7f075ba42c2f0f28e4657
Harshitaonweb <harshitaonweb@gmail.com>
2025-05-21 22:46:55 +0530

    Added Query Selector

--- JS L6/script.js
+++ JS L6/script.js
@@ -4,8 +4,19 @@ window.console.log("hello2");
 
 
 let heading = document.getElementById("heading"); //h1
-console.dir(heading);*/
+console.dir(heading);
 
 let headings = document.getElementsByClassName("heading-class");
 console.dir(headings);
 console.log(headings);
+*/
+
+//Query Selector
+let firstEl = document.querySelector("p"); //for 1st element
+console.dir(firstEl);
+
+let allEl = document.querySelectorAll(".heading-class"); //for all elements
+console.dir(allEl);
+
+let FirstEl = document.querySelector("#myId"); //for 1st element
+console.dir(FirstEl);
