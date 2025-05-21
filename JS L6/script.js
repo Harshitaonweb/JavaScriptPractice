ab5e2971db7d9a4d68a83139856b1fa093e04dd2
Harshitaonweb <harshitaonweb@gmail.com>
2025-05-21 22:32:16 +0530

    Commited JS L6

---
+++ JS L6/script.js
@@ -0,0 +1,11 @@
+/*
+console.log("hello");
+window.console.log("hello2");
+
+
+let heading = document.getElementById("heading"); //h1
+console.dir(heading);*/
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
