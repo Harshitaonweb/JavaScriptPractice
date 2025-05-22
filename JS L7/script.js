5c313325eec7b81331fd51f4df2f70e780eafede
Harshitaonweb <harshitaonweb@gmail.com>
2025-05-22 21:37:52 +0530

    Commited JS L7

---
+++ JS L7/script.js
@@ -0,0 +1,18 @@
+/*
+let div = document.querySelector("div");
+console.log(div);
+
+let idValue = div.getAttribute("id");
+console.log(idValue);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
+
+//setAttribute
+console.log(para.setAttribute("class","newClass"));
+*/
+
+let div = document.querySelector("div");
